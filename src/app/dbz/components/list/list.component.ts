56f922b85476593ace33990f30a8db61f97bb3ed
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2023-09-12 11:52:25 -0500

    fin seccion 5

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ]
+
+  // onDelete = Index value : number
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id: string): void {
+    // TODO: Emitir el ID del personaje
+    // console.log({index});
+
+    if ( !id ) return;
+
+    // console.log( {id} )
+
+    this.onDelete.emit(id);
+  }
+
+}
